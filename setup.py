1cfa59d9536262de5d55695dca1275db66ea0476
Ayato964 <chtk20063@g.nihon-u.ac.jp>
2024-09-16 22:24:48 +0900

    Ver.0.12.10
    学習率の調整

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='MORTM',
-    version='0.12.2',
+    version='0.12.10',
     author='Nagoshi Takaaki',
     author_email='nagoshi@kthrlab.jp',
     description='音楽の旋律生成を実現したシステム',
